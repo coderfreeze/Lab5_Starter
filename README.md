b725ab510c6e18ddf254639dac86b4c0c7ab2bd4
coderfreeze <vijaymastern@gmail.com>
2025-05-04 23:03:19 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Lab 5 - Starter
+# Vijay Sinha  
+  
+1) No, you would not unit test the messaging feature. A messaging feature involves many different  
+components to make sure the message sends properly. Testing it unit by unit does not fully test if the full message feature works properly.   
+  
+
+2) Yes, you would use a unit test to check if the max length feature works properly. We would unit test this because   
+it is a simple check on a message before it sends. It does not involve many different pieces of code to interact with each other such as networking and recipient check.  
+  
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,102 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('Checks valid phone number', () => {
+  // TODO
+  expect(isPhoneNumber('703-482-3929')).toBe(true);
+});
+
+test('Checks valid phone number', () => {
+  // TODO
+  expect(isPhoneNumber('703-331-3479')).toBe(true);
+});
+
+test('Checks invalid phone number', () => {
+  // TODO
+  expect(isPhoneNumber('72-29-991')).toBe(false);
+});
+
+test('Checks invalid phone number', () => {
+  // TODO
+  expect(isPhoneNumber('921-3')).toBe(false);
+});
+
+test('Checks valid email', () => {
+  // TODO
+  expect(isEmail('vijay@gmail.com')).toBe(true);
+});
+
+test('Checks valid email', () => {
+  // TODO
+  expect(isEmail('email@email.com')).toBe(true);
+});
+
+test('Checks invalid email', () => {
+  // TODO
+  expect(isEmail('notanemailgmail.com')).toBe(false);
+});
+
+test('Checks invalid email', () => {
+  // TODO
+  expect(isEmail('email@gmail')).toBe(false);
+});
+
+test('Checks strong password', () => {
+  // TODO
+  expect(isStrongPassword('password102_')).toBe(true);
+});
+
+test('Checks strong password', () => {
+  // TODO
+  expect(isStrongPassword('strongpass3099')).toBe(true);
+});
+
+test('Checks weak password', () => {
+  // TODO
+  expect(isStrongPassword('1pass')).toBe(false);
+});
+
+test('Checks weak password', () => {
+  // TODO
+  expect(isStrongPassword('password@')).toBe(false);
+});
+
+test('Checks valid date', () => {
+  // TODO
+  expect(isDate('02/09/1999')).toBe(true);
+});
+
+test('Checks valid date', () => {
+  // TODO
+  expect(isDate('07/12/2025')).toBe(true);
+});
+
+test('Checks invalid date', () => {
+  // TODO
+  expect(isDate('9/8/7')).toBe(false);
+});
+
+test('Checks invalid date', () => {
+  // TODO
+  expect(isDate('/7/2019')).toBe(false);
+});
+
+test('Checks valid hex color', () => {
+  // TODO
+  expect(isHexColor('#0000ff')).toBe(true);
+});
+
+test('Checks valid hex color', () => {
+  // TODO
+  expect(isHexColor('FF00FF')).toBe(true);
+});
+
+test('Checks invalid hex color', () => {
+  // TODO
+  expect(isHexColor('G$GJEO31')).toBe(false);
+});
+
+test('Checks invalid hex color', () => {
+  // TODO
+  expect(isHexColor('HEXHEX')).toBe(false);
+});
